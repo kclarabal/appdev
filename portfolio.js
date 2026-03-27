480cada84ca92a6e6b2806c0c039f39f97870bb5
kclarabal <kclarabal@addu.edu.ph>
2026-03-27 19:12:26 +0800

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,6 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
 
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
