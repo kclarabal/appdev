6373ee326656658341b5f7e4fe81e817d1258416
kclarabal <kclarabal@addu.edu.ph>
2026-03-27 19:42:36 +0800

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,22 @@
+
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
+});
+
+
+const faders = document.querySelectorAll(".fade");
+
+const appearOnScroll = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
 });
